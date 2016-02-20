3c954c85c807b01e085f3f7a880ab85c8075fcdf
Brian Merchant <btmerchant@comcast.net>
2016-02-20 10:48:32 -0600

    Basic I/O working

---
+++ csharp/ConsoleApplication1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name");
+            var name = Console.ReadLine();
+            Console.WriteLine("Thank You " + name);
+            var go = Console.ReadKey();
+        }
+    }
+}
