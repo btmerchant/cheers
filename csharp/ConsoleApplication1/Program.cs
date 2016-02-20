155b263294272ef563af46501693496e2ae82d19
Brian Merchant <btmerchant@comcast.net>
2016-02-20 12:56:41 -0600

    Program step 1 working

--- csharp/ConsoleApplication1/Program.cs
+++ csharp/ConsoleApplication1/Program.cs
@@ -12,8 +12,34 @@ namespace ConsoleApplication1
         {
             Console.WriteLine("What is your name");
             var name = Console.ReadLine();
+            var nameS = name.ToString();
+            int nameI = nameS.Length;
+            char[] nameA = name.ToArray(); 
             Console.WriteLine("Thank You " + name);
+            for (int i = 0; i < nameI; i++)
+                {
+                bool vowelTestResult = TestForVowel(nameS[i]);
+                    if (vowelTestResult)
+                    {
+                        Console.WriteLine("Giveme an " + nameS[i]);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Giveme a " + nameS[i]);
+                    }
+                }
+            Console.WriteLine("Press any key to exit");         
             var go = Console.ReadKey();
+            }
+        public static bool TestForVowel(char c)
+        {
+            string vowels = "aeio";
+            bool b = false;
+                if (vowels.Contains(c))
+                { b = true; }
+                else
+                { b = false; }
+            return b;
         }
     }
 }
